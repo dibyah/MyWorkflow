ca0d06dc8eb924b6ac2a30c894aef2469812ba5e
Dibya Hazra <96776869+dibyah@users.noreply.github.com>
2022-05-11 18:02:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
 
+resource "azurerm_managed_disk" "my-managed-disk" {
+  create_option        = "Empty"
+  location             = "California"
+  name                 = "my-managed-disk-1"
+  resource_group_name  = "some-rg"
+  storage_account_type = "Standard_LRS"
+}
